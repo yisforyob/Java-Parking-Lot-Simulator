8f4a404f91df51e9c4162d36c007d06da61c1fa6
Ahmed Rebai <115375494+AhmedRebai-1@users.noreply.github.com>
2023-01-31 09:08:04 -0500

    Create Car.java

---
+++ Car.java
@@ -0,0 +1,75 @@
+/**
+ * @author Mehrdad Sabetzadeh, University of Ottawa
+ */
+public class Car {
+
+	/**
+	 * Instance variable for storing the type of the car
+	 */
+	private CarType type;
+
+	/**
+	 * Instance variable for storing the car plate number
+	 */
+	private String plateNum;
+
+	/**
+	 * @return the type of this car
+	 */
+	public CarType getType() {
+		// WRITE YOUR CODE HERE!
+		return type;
+	}
+
+	/**
+	 * Sets the type of the car
+	 * 
+	 * @param type is the car type
+	 */
+	public void setType(CarType type) {
+		// WRITE YOUR CODE HERE!
+		type = type;
+	}
+
+	/**
+	 * @return the plate number
+	 */
+	public String getPlateNum() {
+		// WRITE YOUR CODE HERE!
+		return plateNum;
+	}
+
+	/**
+	 * Sets the car plate number
+	 * 
+	 * @param plateNum is the car plate number
+	 */
+	public void setPlateNum(String plateNum) {
+		// WRITE YOUR CODE HERE!
+		plateNum = plateNum;
+	}
+
+	/**
+	 * Constructor for Car
+	 * 
+	 * @param type     is the type of the car
+	 * @param plateNum is the car plate number
+	 */
+	public Car(CarType typ, String plateNu) {
+		// WRITE YOUR CODE HERE!
+		CarType type;
+		type = typ;
+		String plateNum;
+		plateNum = plateNu;
+		
+	}
+
+	/**
+	 * Returns a string representation of the car
+	 */
+	public String toString() {
+		// NOTE: The implementation of this method is complete. You do NOT need to
+		// change it for the assignment.
+		return Util.getLabelByCarType(type) + '(' + plateNum + ')';
+	}
+}
