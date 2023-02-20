0273dbcb11d27f68794a03799affcf7f2ba5620e
Ahmed Rebai <115375494+AhmedRebai-1@users.noreply.github.com>
2023-02-20 16:57:36 -0500

    Completed Spot.java

--- Spot.java
+++ Spot.java
@@ -2,42 +2,36 @@
  * @author Mehrdad Sabetzadeh, University of Ottawa
  *
  */
+
 public class Spot {
 	private Car car;
 	private int timestamp;
 
 	public Car getCar() {
-
 		// WRITE YOUR CODE HERE!
-
-		return null; // Remove this statement when your implementation is complete.
-
+		return this.car;
 	}
 
 	public void setCar(Car car) {
-	
 		// WRITE YOUR CODE HERE!
-	
+		this.car=car;
+		this.timestamp=getTime();
 	}
 
 	public int getTimestamp() {
-	
 		// WRITE YOUR CODE HERE!
-		
-		return -1; // Remove this statement when your implementation is complete.
-
+		return this.timestamp;
 	}
 
 	public void setTimestamp(int timestamp) {
-
 		// WRITE YOUR CODE HERE!
-		
+		this.timestamp=timestamp;
 	}
 
 	public Spot(Car car, int timestamp) {
-
 		// WRITE YOUR CODE HERE!
-		
+		this.car=car;
+		this.timestamp=timestamp;
 	}
 
 	/**
